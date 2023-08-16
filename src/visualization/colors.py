6be5c386ffbf6515d969a79f812b0ad48467bed3
Scott Laing <fifersheep@gmail.com>
2023-08-16 01:40:50 +0100

    update colors for taylor swift exploration

--- src/visualization/colors.py
+++ src/visualization/colors.py
@@ -3,18 +3,28 @@ orange_lighten_2 = "#EFBF7E"
 yellow_lighten_2 = "#FFE065"
 green_lighten_2 = "#8ABD92"
 blue_lighten_2 = "#6E9FCD"
-purple_lighten_2 = "#ADA1CF"
 
 red_lighten_1 = "#E26F6F"
 orange_lighten_1 = "#EBB262"
 yellow_lighten_1 = "#FFDA44"
 green_lighten_1 = "#71AF7A"
 blue_lighten_1 = "#4F8BC3"
-purple_lighten_1 = "#9C8DC4"
 
 red = "#DD5555"
 orange = "#E8A446"
 yellow = "#FFD323"
 green = "#58A163"
 blue = "#3076B8"
-purple = "#8A79BA"
+
+purples = {
+    "0": "#F1EFF7",
+    "1": "#DCD7EA",
+    "2": "#C5BCDD",
+    "3": "#ADA1CF",
+    "4": "#9C8DC4",
+    "5": "#8A79BA",
+    "6": "#8271B3",
+    "7": "#7766AB",
+    "8": "#6D5CA3",
+    "9": "#5A4994",
+}

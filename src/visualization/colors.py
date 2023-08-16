bf109eb1b87b18b57d7e9b04c0269b4a4188b581
Scott Laing <fifersheep@gmail.com>
2023-08-16 22:42:48 +0100

    add color palette

--- src/visualization/colors.py
+++ src/visualization/colors.py
@@ -1,20 +1,67 @@
-red_lighten_2 = "#E78888"
-orange_lighten_2 = "#EFBF7E"
-yellow_lighten_2 = "#FFE065"
-green_lighten_2 = "#8ABD92"
-blue_lighten_2 = "#6E9FCD"
+reds = {
+    "0": "#FBEBEB",
+    "1": "#F5CCCC",
+    "2": "#EEAAAA",
+    "3": "#E78888",
+    "4": "#E26F6F",
+    "5": "#DD5555",
+    "6": "#D94E4E",
+    "7": "#D44444",
+    "8": "#CF3B3B",
+    "9": "#C72A2A",
+}
 
-red_lighten_1 = "#E26F6F"
-orange_lighten_1 = "#EBB262"
-yellow_lighten_1 = "#FFDA44"
-green_lighten_1 = "#71AF7A"
-blue_lighten_1 = "#4F8BC3"
+oranges = {
+    "0": "#FCF4E9",
+    "1": "#F8E4C8",
+    "2": "#F4D2A3",
+    "3": "#EFBF7E",
+    "4": "#EBB262",
+    "5": "#E8A446",
+    "6": "#E59C3F",
+    "7": "#E29237",
+    "8": "#DE892F",
+    "9": "#D87820",
+}
 
-red = "#DD5555"
-orange = "#E8A446"
-yellow = "#FFD323"
-green = "#58A163"
-blue = "#3076B8"
+yellows = {
+    "0": "#FFFAE5",
+    "1": "#FFF2BD",
+    "2": "#FFE991",
+    "3": "#FFE065",
+    "4": "#FFDA44",
+    "5": "#ffd323",
+    "6": "#FFCE1F",
+    "7": "#FFC81A",
+    "8": "#FFC215",
+    "9": "#FFB70C",
+}
+
+greens = {
+    "0": "#EBF4EC",
+    "1": "#CDE3D0",
+    "2": "#ACD0B1",
+    "3": "#8ABD92",
+    "4": "#71AF7A",
+    "5": "#58A163",
+    "6": "#50995B",
+    "7": "#478F51",
+    "8": "#3D8547",
+    "9": "#2D7435",
+}
+
+blues = {
+    "0": "#E6EFF6",
+    "1": "#C1D6EA",
+    "2": "#98BBDC",
+    "3": "#6E9FCD",
+    "4": "#4F8BC3",
+    "5": "#3076B8",
+    "6": "#2B6EB1",
+    "7": "#2463A8",
+    "8": "#1E59A0",
+    "9": "#134691",
+}
 
 purples = {
     "0": "#F1EFF7",
@@ -28,3 +75,13 @@ purples = {
     "8": "#6D5CA3",
     "9": "#5A4994",
 }
+
+red = reds["5"]
+orange = oranges["5"]
+yellow = yellows["5"]
+green = greens["5"]
+blue = blues["5"]
+
+dark_grey = "#1E1E2A"
+white_semi_trans = "#FFFFFFE6"
+black_semi_trans = "#00000096"

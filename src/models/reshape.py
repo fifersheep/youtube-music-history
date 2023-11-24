8d9f12a3bce825068aaf24c2e6e8c21ccd66b3b2
Scott Laing <fifersheep@gmail.com>
2023-11-24 23:10:44 +0000

    data transformer + purple styling for overview

--- src/models/reshape.py
+++ src/models/reshape.py
@@ -1,14 +1,16 @@
 import pandas as pd
 
-def data_for_timeframe(listen_history, n_months):
-    # get the most recent date
-    most_recent_date = listen_history["date"].max()
-    
-    # set a timeframe to filter the data
-    timeframe = most_recent_date - pd.DateOffset(months=n_months)
-    
-    # query rows which are within the given timeframe
-    data = listen_history.query("date > @timeframe").copy()
-    
-    return data
 
+class DataTransformer:
+    def get_data(self) -> pd.DataFrame:
+        return pd.read_parquet("../data/processed/listen_history.parquet.gzip")
+
+    def data_for_timeframe(self, data: pd.DataFrame, n_months: int) -> pd.DataFrame:
+        # get the most recent date
+        most_recent_date = data["date"].max()
+
+        # set a timeframe to filter the data
+        timeframe = most_recent_date - pd.DateOffset(months=n_months)
+
+        # query rows which are within the given timeframe
+        return data.query("date > @timeframe").copy()
--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -5,13 +5,27 @@ import matplotlib.font_manager as fm
 import seaborn as sns
 
 def change_matplotlib_font():
-    FONT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'assets'))
-    
+    FONT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "assets"))
+
     font_files = fm.findSystemFonts(fontpaths=[FONT_PATH])
     for font_file in font_files:
         fm.fontManager.addfont(font_file)
 
     font_name = fm.FontProperties(fname=font_files[0]).get_name()
-    matplotlib.rc('font', family=font_name)
-    print("font family: ", plt.rcParams['font.family'])
+    matplotlib.rc("font", family=font_name)
+    print("font family: ", plt.rcParams["font.family"])
+
+
+def seaborn_style(background_color):
+    font_family = "Montserrat"
+    text_color = dark_grey
+
+    sns.set_style(
+        {
+            "axes.facecolor": background_color,
+            "figure.facecolor": background_color,
+            "font.family": font_family,
+            "text.color": text_color,
+        }
+    )
 
